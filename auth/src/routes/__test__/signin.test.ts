909265d8b0fdd65863b8e765dbc26027f0056a63
Mikael MOREAU <mikaelmoreau@yahoo.fr>
2021-02-24 19:11:39 +0100

    added unit tests for authentication

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("fails when a email that doesnt exit is supply", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("fails when an icorrect password is supplied", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "ldskfgn",
+    })
+    .expect(400);
+});
+
+it("respond with a cooki when given valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
